9bd90dd084c33e9269987f60b1d7ced48bc543da
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-11-03 12:50:14 -0300

    comments and better vars

--- src/Modules/InputData.F90
+++ src/Modules/InputData.F90
@@ -3,7 +3,7 @@
 ! calc.
 module inputData
     
-    use iso_fortran_env
+    use iso_fortran_env, only: int32, real64
     
     implicit none
 
@@ -29,30 +29,45 @@ module inputData
     integer(kind=int32),parameter :: nint = 70
 
     ! The numbers on the second line of the flash-parameters-file:
-    !   icalc:  0-' **** FLASH CALCULATION ****'
-    !           1-' **** BINODAL CURVE CALCULATION ****'
-    !           2-' **** CALCULATION OF UNIQUAC PARAMETERS FROM UNIFAC **** '
+    !   icalc:  [0,1,2]     (DEFAULT = 0)
+    !           0-  ' **** FLASH CALCULATION ****'
+    !           1-  ' **** BINODAL CURVE CALCULATION ****'
+    !           2-  ' **** CALCULATION OF UNIQUAC PARAMETERS FROM UNIFAC **** '
     !
-    !   model:  0-' MODEL USED FOR LIQUID PHASE NON-IDEALITY: UNIFAC'
-    !           1-' MODEL USED FOR LIQUID PHASE NON-IDEALITY: UNIQUAC'
-    !           2-' MODEL USED FOR LIQUID PHASE NON-IDEALITY: A-UNIFAC'
-    !           3-' MODEL USED FOR GROUP CONTRIBUTION MODE: GC'
+    !   model:  [0,1,2,3]   (DEFAULT = 0)
+    !           0-  ' MODEL USED FOR LIQUID PHASE NON-IDEALITY: UNIFAC'
+    !           1-  ' MODEL USED FOR LIQUID PHASE NON-IDEALITY: UNIQUAC'
+    !           2-  ' MODEL USED FOR LIQUID PHASE NON-IDEALITY: A-UNIFAC'
+    !           3-  ' MODEL USED FOR GROUP CONTRIBUTION MODE: GC'
     !
-    !   ipr:    1-' ** COMPARISON OF ACTIVITIES CALCULATED BY UNIFAC AND 
-    !                UNIQUAC, RESPECTIVELY **'
+    !   ipr:    [0,1]       (DEFAULT = 0)
+    !           1-  ' ** COMPARISON OF ACTIVITIES CALCULATED BY UNIFAC AND 
+    !               UNIQUAC, RESPECTIVELY **'
     !
-    !   iout:   1-'open 'lleasoccuzada.OUT''
+    !   iout:   [0,1]       (DEFAULT = 1)
+    !           1-  'open 'lleasoccuzada.OUT''
     !
-    !   novap:  0-'VAPOR PHASE INCLUDED IN FLASH-CALCULATIONS'
+    !   novap:  [0,1]       (DEFAULT = 0)
+    !           0-'VAPOR PHASE INCLUDED IN FLASH-CALCULATIONS'
     !
-    !   ig:     0-'write compositions'
-    !           1-'write compositions and activities'
+    !   ig:     [0,1]       (DEFAULT = 1)
+    !           0-  'write compositions'
+    !           1-  'write compositions and activities'
     !
-    !   ipareq: 1-'liquid-liquid parameters table (UNIFAC)'
+    !   ipareq: [0,1]       (DEFAULT = 2)
+    !           1-'liquid-liquid parameters table (UNIFAC)'
     !           2-'liquid-vapor parameters table (UNIFAC)'
     !           3-'infinite dilution parameters table (UNIFAC)'
     !           4-'GC-EOS parameters'
-    integer(kind=int32) :: ipareq, icalc, model, ipr, iout, novap, ig
+    !
+    integer(kind=int32) :: &
+        & icalc = 0,& 
+        & model = 0,& 
+        & ipr = 0,& 
+        & iout = 1,& 
+        & novap = 0,& 
+        & ig = 1,&
+        & ipareq = 2 
 
     integer(kind=int32) :: output
     
@@ -67,7 +82,7 @@ module inputData
     
     ! A matrix with the size of (the max site number per group x
     ! Max size of the arrays which contains subgroups' data)
-    real(kind=real64), dimension(NMG,NMG) :: aint1
+    real(kind=real64), dimension(NMG,NMG) :: aint1 = 0.D0
 
     ! A string with the first line of the flash calc input file. It is usually
     ! the same name in the namefile
@@ -75,7 +90,7 @@ module inputData
     !characterlen=2), dimension(name_maxlen) :: NTEXT
     character(len=name_maxlen) :: ntext
     
-    real(kind=real64), dimension(10,3):: ant
+    real(kind=real64), dimension(10,3):: ant = 0.D0
     
     ! The original InputData module content:
     !integer,parameter::NMS = 2 !n�mero m�ximo de sitios por grupo
@@ -85,4 +100,4 @@ module inputData
     !integer::output
     !real*8,dimension(NMG,NMG)::aint1
 
-endmodule inputData
+end module inputData
