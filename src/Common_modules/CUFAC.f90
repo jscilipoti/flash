33da31fe9d85537da2b2d96d20d0054f4f729e32
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-11-06 16:52:55 -0300

    0.D0

--- src/Common_modules/CUFAC.f90
+++ src/Common_modules/CUFAC.f90
@@ -10,9 +10,9 @@ module CUFAC
  
     ! These are module variables that can be used by any program unit that uses 
     ! this module
-    integer(kind=int32) :: NKK, NGG
-    real(kind=real64) :: Txx
-    real(kind=real64), dimension(10,10) :: Pxx
+    integer(kind=int32) :: NKK = 0.0, NGG = 0.0
+    real(kind=real64) :: Txx = 0.D0
+    real(kind=real64), dimension(10,10) :: Pxx = 0.D0
     
   
   end module CUFAC
