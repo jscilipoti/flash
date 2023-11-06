1897831df7b7482c04c620308e3a9567ccfe4305
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-11-06 12:56:40 -0300

    comments

--- src/Main_SUB/get_free_unit.f90
+++ src/Main_SUB/get_free_unit.f90
@@ -1,4 +1,4 @@
-! A subroutine that returns a free unit number
+! A subroutine that returns a free unit number to open a file.
 subroutine get_free_unit(unit)
 use iso_fortran_env, only: int8, int16
 implicit none
@@ -6,7 +6,7 @@ implicit none
 integer(kind=int8) :: unit
 integer(kind=int16) :: iostat
 logical :: opened
-do unit = 10, 126 ! Loop over possible unit numbers
+do unit = 10, 126 ! Loop over possible unit numbers (126 is int8 max number)
   inquire (unit=unit, opened=opened, iostat=iostat) ! Check if the unit is open
   if (iostat /= 0) cycle ! Skip if there is an error
   if (.not. opened) exit ! Exit the loop if the unit is free
