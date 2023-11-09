bee220ed60d943a09df8bc6baf18fcc523716504
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-11-09 00:14:29 -0300

    Modern Fortran Standards

--- src/Fuctions_SUB/mainsgfunc.f90
+++ src/Fuctions_SUB/mainsgfunc.f90
@@ -1,16 +1,33 @@
-integer function mainsgfunc (i1,ipareq)
-!c-------------------------------------------------------------------
-!c      Esta funcion devuelve el numero de grupo principal correspon-
-!c      diente al subgrupo i1 de la tabla de parametros ipareq:
-!c                     1: liquido-liquido
-!c                     2: liquido-vapor
-!c                     3: dilucion-infinita
-!c-------------------------------------------------------------------
-!c
-    integer:: il,ipareq
-	irec1=i1+(ipareq-1)*150
-      read (14,10,rec=irec1) main
-      mainsgfunc = main
-  10  format (i4)
-      return
-endfunction mainsgfunc
+integer function mainsgfunc (i, ipareq)
+
+!-------------------------------------------------------------------------------
+!       This function returns the main group number corresponding to the 
+!       subgroup "i" of the "ipareq" parameter table:
+!           1: liquid-liquid
+!           2: liquid-vapor
+!           3: infinite-dilution
+!       The function calculates the record number to read from by adding i 
+!       to the product of ipareq and 150, which is the number of records 
+!       per parameter table. It assumes that unit 14 (gruposram.mds) 
+!       has been opened and positioned correctly before calling it.
+!       The inputs are "i", which is the record number within a parent request, 
+!       and "ipareq", which is the parameter table number.
+!       The output is mainsgfunc, which is the main group number.
+!-------------------------------------------------------------------------------
+
+    use iso_fortran_env, only: int8, int32
+
+    implicit none
+
+    integer(kind = int8), intent(in) :: i, ipareq
+    integer(kind = int8) :: main_group
+    integer(kind = int32) :: read_record
+
+	read_record = i + (ipareq - 1) * 150
+    
+    read (14, '(i4)', rec = read_record) main_group
+    
+    mainsgfunc = main_group
+    return
+
+end function mainsgfunc
