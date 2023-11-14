2244c6da2a7d1186e80ee8c064c724d370c93eed
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-11-14 14:24:19 -0300

    better param2

--- src/LLECALAS_SUB/param2.f90
+++ src/LLECALAS_SUB/param2.f90
@@ -1,20 +1,43 @@
-SUBROUTINE PARAM2                                                 
-    IMPLICIT REAL*8(A-H,O-Z)                                          
-    COMMON/CUFAC/NK,NG,P(10,10),T                                     
-    COMMON/CPAR/TAU(10,10),S(10,10),F(10)                             
-    COMMON/CQT/QT(10,10),Q(10),R(10)                                  
-    DO 30 I=1,NG                                                      
-      DO 30 J=1,NG                                                      
- 30       TAU(I,J)=DEXP(-P(I,J)/T)                                          
- 40 CONTINUE                                                          
-    DO 50 I=1,NK                                                      
-      DO 50 K=1,NG                                                      
-          S(K,I)=0.D0                                                       
-    DO 50 M=1,NG                                                      
- 50   S(K,I)=S(K,I)+QT(M,I)*TAU(M,K)                                    
-    DO 60 I=1,NK                                                      
-      F(I)=1.D0                                                         
-    DO 60 J=1,NG                                                      
- 60   F(I)=F(I)+QT(J,I)*DLOG(S(J,I))                                    
-    RETURN                                                            
-    END
+subroutine param2
+    use iso_fortran_env, only: int32, real64                                                
+    !IMPLICIT REAL * 8(A-H, O-Z)
+    implicit none
+
+    COMMON/CUFAC/nk, ng, P, T                                     
+    COMMON/CPAR/TAU, S, F                            
+    COMMON/CQT/QT, Q, R
+
+    integer(kind = int32) :: i, j, k
+    integer(kind = int32) :: ng, nk
+    real(kind = real64), dimension(10,10) :: P, QT, TAU, S
+    real(kind = real64), dimension(10) :: F
+    real(kind = real64) :: T
+
+    ! NOT USED
+    real(kind = real64), dimension(10) :: Q, R
+    ! ----
+
+    do i = 1, ng                                                      
+        do j = 1, ng                                                      
+            TAU(i, j) = DEXP(-P(i, j) / T)
+        end do
+    end do
+
+    do i = 1, nk                                                      
+        do j = 1, ng                                                      
+            S(j, i) = 0.D0                                                       
+            do k = 1, ng                                                      
+                S(j, i) = S(j, i) + QT(k, i) * TAU(k, j)
+            end do
+        end do
+    end do                                    
+    do i = 1, nk                                                      
+        F(i) = 1.D0                                                         
+        do j = 1, ng                                                      
+            F(i) = F(i) + QT(j, i) * DLOG(S(j, i))
+        end do
+    end do
+    
+    return
+end subroutine param2
+
